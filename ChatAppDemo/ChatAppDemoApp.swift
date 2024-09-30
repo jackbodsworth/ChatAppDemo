ed30ce5071f598c2c50bf96cc58466560ce39d13
Jack Bodworth <jack.bodsworth@unimelb.edu.au>
2024-09-30 13:41:11 +1000

    Initial Commit

---
+++ ChatAppDemo/ChatAppDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatAppDemoApp.swift
+//  ChatAppDemo
+//
+//  Created by Jack Bodsworth on 30/9/2024.
+//
+
+import SwiftUI
+
+@main
+struct ChatAppDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ChatAppDemoTests/ChatAppDemoTests.swift
@@ -0,0 +1,17 @@
+//
+//  ChatAppDemoTests.swift
+//  ChatAppDemoTests
+//
+//  Created by Jack Bodsworth on 30/9/2024.
+//
+
+import Testing
+@testable import ChatAppDemo
+
+struct ChatAppDemoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
